1688ff96e8082ade9aa83dc480590dedb90faf38
coka13 <isafeze@gmail.com>
2024-01-25 21:08:13 +0200

    added request

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-const changeStyle=document.querySelector(".style");
-changeStyle.addEventListener("click",(e)=>{
-    document.body.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);
-})
+let imgSrc = "";
+const div = document.createElement("div")
+
+
+const getPokemon = async (url) => {
+    try {
+        div.innerHTML=""
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+
+        imgSrc = data.sprites.front_default;
+        const img = document.createElement("img");
+        img.src = imgSrc;
+        div.appendChild(img);
+    } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+    }
+};
+
+const makeRequest = document.querySelector(".request");
+makeRequest.addEventListener("click", (e) => {
+    const pokemon_id = Math.floor(Math.random() * 898);
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id}`;
+    
+    getPokemon(url);
+});
+document.body.appendChild(div)
