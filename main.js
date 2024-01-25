fd9766951c9368995d92c1d396578f5591a352d7
coka13 <isafeze@gmail.com>
2024-01-25 21:20:31 +0200

    fixed all this shit

--- main.js
+++ main.js
@@ -26,3 +26,8 @@ makeRequest.addEventListener("click", (e) => {
     getPokemon(url);
 });
 document.body.appendChild(div)
+
+const changeStyle=document.querySelector(".style");
+changeStyle.addEventListener("click",(e)=>{
+    document.body.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);
+})
