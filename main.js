dd017a90e9a72e0227dba187ccc461c8cc29f8e0
coka13 <isafeze@gmail.com>
2024-01-25 21:48:45 +0200

    fixed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let imgSrc = "";
 const div = document.createElement("div")
-
+const makeRequest = document.querySelector(".request");
 
 const getPokemon = async (url) => {
     try {
@@ -8,26 +8,28 @@ const getPokemon = async (url) => {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
-
         imgSrc = data.sprites.front_default;
         const img = document.createElement("img");
         img.src = imgSrc;
         div.appendChild(img);
+        makeRequest.disabled=false
     } catch (error) {
         console.error("Error fetching Pokemon data:", error);
     }
+
 };
 
-const makeRequest = document.querySelector(".request");
+
 makeRequest.addEventListener("click", (e) => {
+    makeRequest.disabled=true
     const pokemon_id = Math.floor(Math.random() * 898);
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id}`;
-    
     getPokemon(url);
 });
 document.body.appendChild(div)
 
 const changeStyle=document.querySelector(".style");
 changeStyle.addEventListener("click",(e)=>{
-    document.body.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16)
+    console.log("#"+Math.floor(Math.random()*16777215).toString(16))
 })
